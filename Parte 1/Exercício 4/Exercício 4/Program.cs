b2d3ea65e741426527b3f46736ef020008602adb
matheus.souza <matheus.souza@TI-08>
2024-03-19 10:53:08 -0300

    Atualização

---
+++ Parte 1/Exercício 4/Exercício 4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Infomer as horas!");
+        DateTime hora;
+        Console.ReadLine(hora);
+        
+    }
+}
