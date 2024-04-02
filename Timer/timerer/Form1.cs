54c4b8892b4705dde5d3017373f25e75366c69fc
matheus.souza <matheus.souza@TI-08>
2024-04-02 15:00:07 -0300

    Deveres ai

---
+++ Timer/timerer/Form1.cs
@@ -0,0 +1,25 @@
+namespace timerer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
