ca59018f2b3c7396868efeb81d8b23d89fda6a58
matheus emanoel <matheusemanoelgomessouza@hotmail.com>
2024-05-14 21:22:38 -0300

    Codigo do professor. Meu Código.

---
+++ Prog do Professor/Prog do Professor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Manipulador do arquivo:
+        FileStream meuArq = new FileStream("C:\\Users\\mathe\\source\\repos\\Matheus-Emanoel-Souza\\exe.csharp\\Prog do Professor\\Prog do Professor\\numeros.txt", FileMode.Open, FileAccess.Read);
+        StreamReader leitor = new StreamReader(meuArq, Encoding.UTF8);
+
+        int cont = 1, soma = 0;
+        while (!leitor.EndOfStream)
+        {
+            string linhaTxT = leitor.ReadLine();
+            Console.WriteLine("Linha" + cont + ":" + linhaTxT);
+
+            int num = int.Parse(linhaTxT);
+            soma += num;
+
+            cont++;
+        }
+
+        Console.WriteLine("Valor da Soma: {0}", soma);
+
+        leitor.Close();
+        meuArq.Close();
+
+    }
+}
