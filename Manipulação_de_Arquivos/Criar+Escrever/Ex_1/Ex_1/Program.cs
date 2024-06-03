e622dba4da868ff4ab7f67feb66e6f9c3235564e
Matheus Emanoel <152511027+Matheus-Emanoel-Souza@users.noreply.github.com>
2024-06-03 14:53:18 -0300

    Única pasta que vou manter

---
+++ Manipulação_de_Arquivos/Criar+Escrever/Ex_1/Ex_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        String diretorio = @"C:\Users\matheus.souza\Desktop\Algoritmos_Estrutura_de_Dados\Manipulação_de_Arquivos";
+        string filepath = Path.Combine(diretorio, "lista_de_vilões.txt");
+
+        try
+        {
+            if (!Directory.Exists(filepath))
+            {
+                Directory.CreateDirectory(diretorio);
+                Console.WriteLine("diretório criado:" + diretorio);
+            }
+            using (StreamWriter escrever = new StreamWriter(filepath))
+            {
+                escrever.WriteLine("este é o conteudo do arquivo");
+                Console.WriteLine("Arquivo criado com sucesso:" + filepath);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("ocorreu um erro" + ex.Message);
+        }
+    }
+}
---
+++ Manipulação_de_Arquivos/EX_1/ex_1/ex_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+class program
+{
+    static void Main()
+    {
+        string caminho = @"C:\Users\matheus.souza\Desktop\Algoritmos_Estrutura_de_Dados\Manipulação_de_Arquivos\EX_1\ex_1";
+        string caminho_e_carquivo = Path.Combine(caminho+"texto.txt");
+        try
+        {
+            if (!Directory.Exists(caminho_e_carquivo))
+            {
+                Directory.CreateDirectory(caminho);
+                Console.WriteLine("Diretório criado");
+            }
+            else
+            {
+                Console.WriteLine("JÁ EXISTE UM ARQUIVO!!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("ocorreu um erro"+ ex.Message);
+        }
+        using (StreamWriter escreve_no_arquivo = new StreamWriter(caminho_e_carquivo))
+        {
+            escreve_no_arquivo.WriteLine(Console.ReadLine());
+            Console.WriteLine("escrito com sucesso");
+        }
+        
+    }
+}
